f7ea50560a4fb0566b8fda53971444b3258050c2
Davide <dadavidenapolitano@gmail.com>
2021-09-30 16:35:00 +0200

    Header base completo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Header />
+
+    <Main msg=""/>
+
+    <Footer msg=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./styles/general.scss";
+
+  .container {
+    width: 100%;
+    height: 100%;
+    // background: black;
+  }
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,200 @@
+<template>
+  <header>
+      <div class="backgroundHeader">
+         <div class="scuro"></div>
+          <div class="headerTop">
+              <div class="logo">
+                  <img src="../img/logo-light.png" alt="">
+              </div>
+              <nav class="navigazione">
+                  <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a> {{link.text}} </a>
+                    </li>
+                  </ul>
+              </nav>
+              <div class="icone">
+                qui vanno le icone del search hamburger e valiggia
+              </div>
+          </div>
+          <div class="headerCenter">
+              <h1>Contemporary Ideas</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi quis natus totam veniam, commodi dolorum inventore,
+                deleniti quasi laborum obcaecati blanditiis libero voluptatem error aut
+                repudiandae facilis architecto ex? Blanditiis?</p>
+              <div class="register">
+                <div>Register Now</div>
+              </div>
+          </div>
+          <div class="headerBottom">
+              <div v-for="n in 3" :key="n" class="cerchio">
+                  {{n.cerchio}}
+                  <div class="pallino"></div>
+              </div>
+          </div>
+      </div>
+  </header>   
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      links: [
+        {
+          text: "Home",
+          url: "#",
+        },
+        {
+          text: "Courses",
+          url: "#",
+        },
+        {
+          text: "Istructor",
+          url: "#",
+        },
+        {
+          text: "Events",
+          url: "#",
+        },
+        {
+          text: "Pages",
+          url: "#",
+        },
+        {
+          text: "Elements",
+          url: "#",
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+@import '../styles/variables';
+
+  .backgroundHeader {
+    position: relative;
+    width: 100%;
+    height: 900px;
+    background-image: url('../img/course-9-f-img.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .scuro {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgb(6, 6, 6, 0.2);
+  }
+
+  .headerTop {
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+    top: 0%;
+    left: 0%;
+  }
+
+  .logo {
+    width: 20%;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+  }
+
+  .logo > img {
+    width: 100%;
+  }
+
+  .navigazione {
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ul, li {
+    display: flex;
+    list-style: none;
+    margin: 20px;
+    text-transform: uppercase;
+    color: $bianco;
+  }
+
+  .icone {
+    width: 20%;
+  }
+
+ .headerCenter {
+    position: absolute;
+    width: 80%;
+    margin: 0 auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 90px;
+    margin-bottom: 30px;
+    color: $bianco;
+  }
+
+  p {
+    margin-bottom: 30px;
+    color: $bianco;
+  }
+
+  .register {
+    width: 180px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: $azzurro;
+    margin: 0px auto;
+    text-transform: uppercase;
+    color: $bianco;
+  }
+  
+  .headerBottom {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cerchio {
+    width: 20px;
+    height: 20px;
+    border: 2px solid $bianco;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px;
+  }
+ 
+  .pallino {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background: $bianco;
+  }
+
+ 
+</style>
